e756f59263ec59e88490ae6b2ad3abef51755790
Brit Butler <redline6561@gmail.com>
2015-02-16 11:54:38 -0500

    Add games join method.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,16 @@ class GamesController < ApplicationController
   before_action :authenticate_user!, :only => [:join, :move]
 
   def join
+    @waiting = Game.waiting.first
+    if @waiting
+      @waiting.users << current_user
+      redirect_to game_path(@waiting)
+    else
+      @game = Game.create
+      @game.users = [current_user]
+      @game.new_board!
+      redirect_to game_path(@game)
+    end
   end
 
   def move
